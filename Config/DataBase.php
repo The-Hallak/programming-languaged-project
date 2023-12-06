73e8450f8f559456e6f55741c99402977f7abb4d
mohamad alhallak <The-Hallak>
2023-12-06 11:38:37 +0300

    create database and some files

---
+++ Config/DataBase.php
@@ -0,0 +1,74 @@
+<?php
+
+class DataBase{
+    public $host='localhost';
+    private $username='root';
+    private $password='Mohamad@2002';
+    private $dbname='projectsManager';
+    private $connection;
+
+    public function connect(){
+        try {
+            $this->connection = new PDO("mysql:host={$this->host};", $this->username, $this->password);
+            $this->connection->query("USE {$this->dbname}");
+        }catch(PDOException $e){
+            $str="SQLSTATE[42000]";
+            if(substr($e->getMessage(),0,strlen($str))==$str){
+                $this->createDB();
+                $this->connect();
+                return;
+            }
+            echo $e->getMessage();
+        }
+    }
+    
+    private function createDB(){
+
+        $sql="CREATE DATABASE {$this->dbname}";
+        $this->connection->query($sql);
+
+        $sql="USE {$this->dbname}";
+        $this->connection->query($sql);
+
+
+        $sql="CREATE TABLE users(
+            user_id INT AUTO_INCREMENT NOT NULL,
+            full_name VARCHAR(20) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            role INT NOT NULL,
+            PRIMARY KEY(user_id)
+        );";
+        $this->connection->query($sql);
+
+
+        $sql="CREATE TABLE projects(
+            project_id INT AUTO_INCREMENT,
+            name VARCHAR(50) NOT NULL,
+            description VARCHAR(150),
+            teacher_id INT NOT NULL,
+            number_of_student INT NOT NULL,
+            remaining_number_of_student INT NOT NULL,
+            PRIMARY KEY(project_id),
+            FOREIGN KEY(teacher_id)
+                REFERENCES users(user_id)
+        );";
+        $this->connection->query($sql);
+
+        $sql="CREATE TABLE student_project(
+            student_id INT NOT NULL,
+            project_id INT NOT NULL,
+            FOREIGN KEY(student_id)
+                REFERENCES users(user_id),
+            FOREIGN KEY(project_id)
+                REFERENCES projects(project_id)
+        );";
+         $this->connection->query($sql);
+
+
+        $sql="INSERT INTO users(full_name,email,password,role) VALUES('mohamad alhallak','srdamaa777mmm@gmail.com','34269123',0);";
+        $this->connection->query($sql);
+    }
+}
+
+?>
